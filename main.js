965db2af0e4395b134a96f1fdb441b6537bdf192
Simonni <szkiflay@gmal.com>
2018-09-15 14:16:15 -0400

    add image to js

--- main.js
+++ main.js
@@ -12,3 +12,26 @@ console.log(city);
 
 city = "Washington DC";
 console.log(city);
+
+let light = "yellow";
+if (light=="red"){
+    console.log("Stop");
+} else if (light=="yellow"){
+    console.log("slow dawn");
+
+} else {
+    console ("run");
+}
+
+    let welcome = prompt("Welcome to my page");
+     
+    var r = confirm("Press a button!");
+    if (r == true) {
+        txt = "You pressed OK!";
+    } else {
+        txt = "You pressed Cancel!";
+    }
+    console.log (txt);
+
+
+let a=alert("You are smart");
