fb9d959d548b806b94874431379240fad43b6ac4
Simonni <szkiflay@gmal.com>
2018-09-15 11:31:33 -0400

    adding js

---
+++ main.js
@@ -0,0 +1,14 @@
+console.log("Hello World!");
+
+let box = "bottle";
+
+box= "phone";
+
+box = "wire";
+console.log(box);
+
+let city = "Asmara";
+console.log(city);
+
+city = "Washington DC";
+console.log(city);
